e12b4170d814fd6d8066006e97d836772ecd7916
Paulo Roberto <paulo.rqsf@gmail.com>
2022-02-04 16:28:10 -0300

    Comentada a questão 8, modificações nas outras questões

--- Questao_8/Main.java
+++ Questao_8/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main
 {
+    //Procedimento que imprime um vetor de funcionarios, dado um vetor do tipo Funcionario e o seu tamanho.
     public static void printFunc(Funcionario []f, int qtd){
         for(int i=0; i<qtd; i++){
             System.out.println("------------------------------------------");
@@ -15,12 +16,16 @@ public class Main
 
         }
     }
+    //Procedimento que cadastra os funcionarios em um vetor, dado um vetor do tipo Funcionario e o seu tamanho.
     public static void cadastraFunc(Funcionario []f, int qtd){
-
+        //Teclado buffer
         Scanner teclado = new Scanner(System.in);
         for(int i = 0; i<qtd; i++){
+            //Bloco try catch para tratar qualquer erro presente no código, principalmente de input.
             try {
+                //Cria um objeto novo para cada funcionario.
                 f[i] = new Funcionario();
+
                 System.out.format("Digite o nome do %d funcionário: ", i + 1);
                 String nome = teclado.next();
 
@@ -29,35 +34,43 @@ public class Main
 
                 f[i].setNome(nome);
                 f[i].setSalario(salario);
+
             }catch(Exception e){
+                //Caso o input não esteja correto, um erro é exibido na tela, e o for volta 1 vez, para ser digitado
+                // o funcionario que teve as informações erradas.
                 System.out.println("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n ERRO: Digite uma opção válida!\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
                 i--;
-            }finally {
+                //"Limpa" o buffer do teclado.
                 teclado.nextLine();
             }
         }
     }
+    //main
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
         int qtd;
         Funcionario []funcionarios;
 
         while(true){
             System.out.println("Digite a quantidade de funcionários que deseja cadastrar: ");
+            //Bloco try catch para tratar qualquer erro presente no código, principalmente de input.
             try {
                 qtd = teclado.nextInt();
                 funcionarios = new Funcionario[qtd];
 
                 cadastraFunc(funcionarios,qtd);
 
+                //Caso tudo esteja certo, o vetor de funcionarios é criado e o break sai do laço infinito.
                 break;
             }catch(Exception e){
+                //Caso o numero digitado não seja numero, um erro é exibido na tela, e o usuario deve digitar o numero de funcionarios novamente.
                 System.out.println("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n ERRO: Digite uma opção válida!\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
-            }finally {
+                //"Limpa" o buffer do teclado.
                 teclado.nextLine();
             }
         }
-
+        //Chama o procedimento de impressao.
         printFunc(funcionarios, qtd);
     }
 
