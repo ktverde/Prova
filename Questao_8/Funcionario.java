a24ff159358bdd3268a9cb3344173973542bcaf2
Paulo Roberto <paulo.rqsf@gmail.com>
2022-02-06 16:30:17 -0300

    Adicionando ArrayList Questao 8.

--- Questao_8/Funcionario.java
+++ Questao_8/Funcionario.java
@@ -2,17 +2,23 @@ package Questao_8;
 
 public class Funcionario
 {
-    private double salario;
+    private double salario, liquido;
     private String nome;
 
     public Funcionario(){
 
     }
+
+    public Funcionario(double salario, String nome) {
+        this.salario = salario;
+        this.nome = nome;
+    }
+
     //Método que calcula a bonificação e já retorna o salario liquido.
     public double getBonificacao(){
-        if(this.salario <= 1000) return this.salario += this.salario*0.2;
-        else if(this.salario > 1000 && this.salario <= 2000) return this.salario += this.salario*0.1;
-        else return this.salario *= 0.9;
+        if(this.salario <= 1000) return liquido = this.salario +(this.salario*0.2);
+        else if(this.salario > 1000 && this.salario <= 2000) return liquido =  this.salario + (this.salario*0.1);
+        else return liquido = this.salario * 0.9;
     }
 
 //sets
--- Questao_8/Main.java
+++ Questao_8/Main.java
@@ -1,39 +1,35 @@
 package Questao_8;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
 {
     //Procedimento que imprime um vetor de funcionarios, dado um vetor do tipo Funcionario e o seu tamanho.
-    public static void printFunc(Funcionario []f, int qtd){
+    public static void printFunc(ArrayList<Funcionario> f, int qtd){
         for(int i=0; i<qtd; i++){
             System.out.println("------------------------------------------");
-            System.out.println("Nome: "+f[i].getNome());
-            System.out.println("Salario: "+f[i].getSalario());
-            f[i].getBonificacao();
-            System.out.println("Salario Liquido: "+f[i].getSalario());
+            System.out.println("Nome: "+f.get(i).getNome());
+            System.out.println("Salario: "+f.get(i).getSalario());
+            System.out.println("Salario Liquido: "+f.get(i).getBonificacao());
             System.out.println("------------------------------------------");
 
         }
     }
     //Procedimento que cadastra os funcionarios em um vetor, dado um vetor do tipo Funcionario e o seu tamanho.
-    public static void cadastraFunc(Funcionario []f, int qtd){
+    public static void cadastraFunc(ArrayList<Funcionario> f, int qtd){
         //Teclado buffer
         Scanner teclado = new Scanner(System.in);
         for(int i = 0; i<qtd; i++){
             //Bloco try catch para tratar qualquer erro presente no código, principalmente de input.
             try {
-                //Cria um objeto novo para cada funcionario.
-                f[i] = new Funcionario();
-
                 System.out.format("Digite o nome do %d funcionário: ", i + 1);
                 String nome = teclado.next();
 
                 System.out.format("Digite o salário do %d funcionário: ", i + 1);
                 double salario = teclado.nextDouble();
 
-                f[i].setNome(nome);
-                f[i].setSalario(salario);
+                f.add(new Funcionario(salario,nome));
 
             }catch(Exception e){
                 //Caso o input não esteja correto, um erro é exibido na tela, e o for volta 1 vez, para ser digitado
@@ -50,15 +46,14 @@ public class Main
 
         Scanner teclado = new Scanner(System.in);
         int qtd;
-        Funcionario []funcionarios;
+        ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 
         while(true){
             System.out.println("Digite a quantidade de funcionários que deseja cadastrar: ");
             //Bloco try catch para tratar qualquer erro presente no código, principalmente de input.
             try {
-                qtd = teclado.nextInt();
-                funcionarios = new Funcionario[qtd];
 
+                qtd = teclado.nextInt();
                 cadastraFunc(funcionarios,qtd);
 
                 //Caso tudo esteja certo, o vetor de funcionarios é criado e o break sai do laço infinito.
